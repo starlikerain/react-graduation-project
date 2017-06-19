8665b0a973791ba6910a3b5d32170c3b52fb61f3
StarLikeRain <pengyaokang@qq.com>
2017-06-19 16:14:03 +0800

    update

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import './index.css';
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+// registerServiceWorker();
+ReactDOM.render(
+  <h1>Hi, World</h1>,
+  document.getElementById('root')
+);
