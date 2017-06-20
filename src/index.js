0d2c966b2048168b94815e7f37bb9cc9b8622106
StarLikeRain <pengyaokang@qq.com>
2017-06-20 15:05:59 +0800

    intoduce base knowlodge

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
+import './index.css'
+import Welcome from './Welcome' // Welcome component
 
-// ReactDOM.render(<App />, document.getElementById('root'));
-// registerServiceWorker();
 ReactDOM.render(
-  <h1>Hi, World</h1>,
+  <Welcome name="123"/>,
   document.getElementById('root')
-);
+)
+
+// ReactDOM.render(<App />, document.getElementById('root'))
+// registerServiceWorker()
+
+// ReactDOM.render(
+//   <h1>Hi, World</h1>,
+//   document.getElementById('root')
+// )
+
+// function tick() {
+//   const element = (
+//     <div>
+//       <h1>Hello, world!</h1>
+//       <h2>It is {new Date().toLocaleTimeString()}.</h2>
+//     </div>
+//   )
+//   ReactDOM.render(
+//     element,
+//     document.getElementById('root')
+//   )
+// }
+
+// setInterval(tick, 1000)
