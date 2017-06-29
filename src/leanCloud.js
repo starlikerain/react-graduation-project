4d2b23822248d43e6971565e91146bdd31c87519
StarLikeRain <pengyaokang@gmail.com>
2017-06-29 08:27:51 +0800

    LeanCloud signUp logic complete

--- src/leanCloud.js
+++ src/leanCloud.js
@@ -8,3 +8,31 @@ AV.init({
   appId: APP_ID,
   appKey: APP_KEY
 })
+
+export default AV
+
+// leancloud登录逻辑写好 给userDialog.js 使用
+export function signUp(username, password, successFn, errorFn) {
+  // 新建 AVUser 对象实例
+  var user = new AV.User()
+  // 设置用户名
+  user.setUsername(username)
+  // 设置密码
+  user.setPassword(password)
+  // 设置邮箱
+  user.signUp().then(function (loginedUser) {
+    let user = getUserFromAVUser(loginedUser)
+    successFn.call(null, user)
+  }, function (error) {
+    errorFn.call(null, error)
+  })
+  return undefined
+}
+
+function getUserFromAVUser(AVUser) {
+  // 这里的AVUser是前面 new出来的user实例登录后的promise返回参数
+  return {
+    id: AVUser.id,
+    ...AVUser.attributes
+  }
+}
--- src/userDialog.js
+++ src/userDialog.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react'
 import './UserDialog.css'
+import { signUp } from './leanCloud'
 
 export default class UserDialog extends Component {
-  constructor (props) {
+  constructor(props) {
     super(props)
     this.state = {
       selected: 'signUp',
@@ -12,64 +13,81 @@ export default class UserDialog extends Component {
       }
     }
   }
-  switch (e) {
+  switch(e) {
     this.setState({
       selected: e.target.value
     })
   }
-  signUp (e) {}
-  signIn (e) {}
-  changeFormData (key, e) {
-    let stateCopy = JSON.parse(JSON.stringify(this.state)) // 用 JSON 深拷贝
+  /* this.signUp.bind(this)
+   * 在signUpForm的onSubmit使用了    
+  **/
+  signUp(e) {
+    e.preventDefault()
+    let { username, password } = this.state.formData
+    let success = (user) => {
+      console.log(user)
+    }
+    let error = (error) => {
+      console.log(error)
+    }
+    // import { signUp } from './leanCloud'
+    signUp(username, password, success, error)
+  }
+  signIn(e) { }
+  // changeUserName 和 changePassword 的逻辑都在这里
+  changeFormData(key, e) {
+    let stateCopy = JSON.parse(JSON.stringify(this.state)) // 用 JSON 深拷贝, 单独拎出来一个，以免REACT的warning？
     stateCopy.formData[key] = e.target.value
     this.setState(stateCopy)
   }
-  render () {
+  render() {
     let signUpForm = (
-    <form className='signUp' onSubmit={this.signUp.bind(this)}>
-      {/* 注册*/}
-      <div className='row'>
-        <label>
-          用户名
+      <form className='signUp' onSubmit={this.signUp.bind(this)}>
+        {/* 注册*/}
+        <div className='row'>
+          <label>
+            用户名
         </label>
-        <input type='text' value={this.state.formData.username} onChange={this.changeFormData.bind(this, 'username')} />
-        {/* bind 不仅可以绑定 this，还可以绑定第一个参数 */}
-      </div>
-      <div className='row'>
-        <label>
-          密码
+          <input type='text' value={this.state.formData.username} onChange={this.changeFormData.bind(this, 'username')} />
+          {/* bind 不仅可以绑定 this，还可以绑定第一个参数 */}
+        </div>
+        <div className='row'>
+          <label>
+            密码
         </label>
-        <input type='password' value={this.state.formData.password} onChange={this.changeFormData.bind(this, 'password')} />
-      </div>
-      <div className='row actions'>
-        <button type='submit'>
-          注册
+          <input type='password' value={this.state.formData.password} onChange={this.changeFormData.bind(this, 'password')} />
+        </div>
+        <div className='row actions'>
+          <button type='submit'>
+            注册
         </button>
-      </div>
-    </form>
+        </div>
+      </form>
     )
+
     let signInForm = (
-    <form className='signIn' onSubmit={this.signIn.bind(this)}>
-      {/* 登录*/}
-      <div className='row'>
-        <label>
-          用户名
+      <form className='signIn' onSubmit={this.signIn.bind(this)}>
+        {/* 登录*/}
+        <div className='row'>
+          <label>
+            用户名
         </label>
-        <input type='text' value={this.state.formData.username} onChange={this.changeFormData.bind(this, 'username')} />
-      </div>
-      <div className='row'>
-        <label>
-          密码
+          <input type='text' value={this.state.formData.username} onChange={this.changeFormData.bind(this, 'username')} />
+        </div>
+        <div className='row'>
+          <label>
+            密码
         </label>
-        <input type='password' value={this.state.formData.password} onChange={this.changeFormData.bind(this, 'password')} />
-      </div>
-      <div className='row actions'>
-        <button type='submit'>
-          登录
+          <input type='password' value={this.state.formData.password} onChange={this.changeFormData.bind(this, 'password')} />
+        </div>
+        <div className='row actions'>
+          <button type='submit'>
+            登录
         </button>
-      </div>
-    </form>
+        </div>
+      </form>
     )
+
     return (
       <div className='UserDialog-Wrapper'>
         <div className='UserDialog'>
